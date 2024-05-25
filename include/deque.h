75140b7794c9a3c2f525f2b0e7810c6aa34b47fc
cxxuniverse <anshchadha1996@gmail.com>
2024-05-25 20:50:08 +0200

    formatting

--- include/deque.h
+++ include/deque.h
@@ -24,27 +24,22 @@ struct Deque {
   size_t data_size;
 };
 
-bool deque_is_empty(cxc_deque* deque);
-
+cxc_deque* deque_create(size_t data_size);
 node_t* node_create(void* data, size_t data_size);
 
 void node_destroy(node_t* node);
-
-cxc_deque* deque_create(size_t data_size);
-
-void deque_push_front(cxc_deque* deque, void* data);
-
-void deque_push_back(cxc_deque* deque, void* data);
-
 void deque_destroy(cxc_deque* deque);
 
-void deque_pop_front(cxc_deque* deque);
+bool deque_is_empty(cxc_deque* deque);
+
+void deque_push_back(cxc_deque* deque, void* data);
+void deque_push_front(cxc_deque* deque, void* data);
 
 void deque_pop_back(cxc_deque* deque);
+void deque_pop_front(cxc_deque* deque);
+void deque_clear(cxc_deque* deque);
 
 void deque_traverse(cxc_deque* deque, callback_fn cb);
-
-void deque_clear(cxc_deque* deque);
 
 // numbers
 void deque_print_int(node_t* node);
